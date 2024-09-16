ea210bbb9f6af14141e8fea123746135410fc8f5
Stephanie <stephanie.azorsa@unmsm.edu.pe>
2024-09-16 15:41:32 -0500

    Add application test

---
+++ src/test/java/com/encryption/digitalhouse/DigitalhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encryption.digitalhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
